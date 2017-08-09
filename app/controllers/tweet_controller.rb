4af405d59a35f7e05e4abe6b3edf9989b46c181a
nana ikeda <nana@Nana-no-MacBook-Air.local>
2017-08-09 13:31:52 +0900

    投稿機能の実装

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -1,4 +1,11 @@
 class TweetController < ApplicationController
 	def index
+		@tweets = Tweet.all
+		@temp = 32
+	end
+	def new
+	end
+	def create
+		Tweet.create(name: params[:name], image: params[:image], text: params[:text])
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get 'tweet' => 'tweet#index'
+  get 'tweet/new' => 'tweet#new'
+  post 'tweet' => 'tweet#create'
 end
